e571c3e8b00025dfeafd8e8fd3f15a3ff5ce453d
MatthewIsfeld <57026982+MatthewIsfeld@users.noreply.github.com>
2020-02-29 16:22:43 -0500

    Fixed bug where you can push bounceClones during halt

--- DashPunk/Assets/Scripts/BounceCloneScript.cs
+++ DashPunk/Assets/Scripts/BounceCloneScript.cs
@@ -6,11 +6,13 @@ public class BounceCloneScript : MonoBehaviour
 {
     public static int cloneBouncing = 1;
     public ParticleSystem dust;
+    public GameObject playerObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerObject = GameObject.Find("Player");
+        Physics2D.IgnoreCollision(playerObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
     }
 
     // Update is called once per frame
