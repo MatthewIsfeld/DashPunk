eecc01661c1647ea2904c6f67e93d369063bf4cb
David Isfeld <56812996+DavidIsfeld@users.noreply.github.com>
2020-03-17 22:57:21 -0400

    Made Inventory Accessible with I

--- DashPunk/Assets/Scripts/InventoryMenu.cs
+++ DashPunk/Assets/Scripts/InventoryMenu.cs
@@ -9,11 +9,30 @@ public class InventoryMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (PauseMenu.isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
     }
 
-    public void ReturnPauseMenu()
+    public void Resume()
     {
         inventoryDisplayUI.SetActive(false);
+        Time.timeScale = 1f;
+        PauseMenu.isPaused = false;
+    }
+
+    void Pause()
+    {
+        inventoryDisplayUI.SetActive(true);
+        Time.timeScale = 0f;
+        PauseMenu.isPaused = true;
     }
 }
