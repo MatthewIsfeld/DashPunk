5edd0cf1cb74f2c9f8a899e5b1421612cc41a7ec
David Isfeld <56812996+DavidIsfeld@users.noreply.github.com>
2020-03-28 23:06:30 -0400

    Made Player Upgrades Match Text File

--- DashPunk/Assets/Scripts/MainMenu.cs
+++ DashPunk/Assets/Scripts/MainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.IO;
+using System;
 
 public class MainMenu : MonoBehaviour
 {
@@ -15,11 +16,15 @@ public class MainMenu : MonoBehaviour
             writePerm.Write("0,0,0,0,0,0"); //Add a 0, for each upgrade
             writePerm.Close();
         }
-        PlayerUpgrades.maxHealthUp = 0;
-        PlayerUpgrades.clonesUpgrade = 0;
-        PlayerUpgrades.dashCooldownUpgrades = 0;
-        PlayerUpgrades.haltUpgrades = 0;
-        PlayerUpgrades.moveSpeedUpgrade = 0;
+        StreamReader readPerm = new StreamReader(path);
+        string tempCurrencyTxt = readPerm.ReadLine();
+        string[] tempCurrencyTxtList = tempCurrencyTxt.Split(','); // Current length is 6
+        readPerm.Close();
+        PlayerUpgrades.maxHealthUp = 0 + Int32.Parse(tempCurrencyTxtList[1]);
+        PlayerUpgrades.clonesUpgrade = 0 + Int32.Parse(tempCurrencyTxtList[2]);
+        PlayerUpgrades.dashCooldownUpgrades = 0 + Int32.Parse(tempCurrencyTxtList[3]);
+        PlayerUpgrades.haltUpgrades = 0 + Int32.Parse(tempCurrencyTxtList[4]);
+        PlayerUpgrades.moveSpeedUpgrade = 0 + Int32.Parse(tempCurrencyTxtList[5]);
     }
 
     public void PlayGame ()
