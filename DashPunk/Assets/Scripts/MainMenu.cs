40779f9f5fac0562d0a6df37aed91f04c8621b27
David Isfeld <56812996+DavidIsfeld@users.noreply.github.com>
2020-03-28 23:16:25 -0400

    Moved Item Update Code to PlayGame

--- DashPunk/Assets/Scripts/MainMenu.cs
+++ DashPunk/Assets/Scripts/MainMenu.cs
@@ -16,6 +16,11 @@ public class MainMenu : MonoBehaviour
             writePerm.Write("0,0,0,0,0,0"); //Add a 0, for each upgrade
             writePerm.Close();
         }
+    }
+
+    public void PlayGame ()
+    {
+        string path = Application.dataPath + "/PermanentUpgrades.txt";
         StreamReader readPerm = new StreamReader(path);
         string tempCurrencyTxt = readPerm.ReadLine();
         string[] tempCurrencyTxtList = tempCurrencyTxt.Split(','); // Current length is 6
@@ -25,10 +30,6 @@ public class MainMenu : MonoBehaviour
         PlayerUpgrades.dashCooldownUpgrades = 0 + Int32.Parse(tempCurrencyTxtList[3]);
         PlayerUpgrades.haltUpgrades = 0 + Int32.Parse(tempCurrencyTxtList[4]);
         PlayerUpgrades.moveSpeedUpgrade = 0 + Int32.Parse(tempCurrencyTxtList[5]);
-    }
-
-    public void PlayGame ()
-    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
