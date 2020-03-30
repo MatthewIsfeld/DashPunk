079d4737180deb53259e4ec8ce3243a6d44d3b1b
David Isfeld <56812996+DavidIsfeld@users.noreply.github.com>
2020-03-30 19:40:39 -0400

    Added Invincibility Button

--- DashPunk/Assets/Scripts/MainMenu.cs
+++ DashPunk/Assets/Scripts/MainMenu.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.IO;
 using System;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Boolean invincibility;
+    public Text invincibilityTxt;
     void Start()
     {
         Cursor.visible = true;
@@ -17,6 +20,8 @@ public class MainMenu : MonoBehaviour
             writePerm.Write("0,0,0,0,0,0"); //Add a 0, for each upgrade
             writePerm.Close();
         }
+        invincibility = false;
+        invincibilityTxt.color = Color.red;
     }
 
     public void PlayGame ()
@@ -26,12 +31,31 @@ public class MainMenu : MonoBehaviour
         string tempCurrencyTxt = readPerm.ReadLine();
         string[] tempCurrencyTxtList = tempCurrencyTxt.Split(','); // Current length is 6
         readPerm.Close();
-        PlayerUpgrades.maxHealthUp = 0 + Int32.Parse(tempCurrencyTxtList[1]);
+        if (invincibility == false)
+        {
+            PlayerUpgrades.maxHealthUp = 0 + Int32.Parse(tempCurrencyTxtList[1]);
+        } else
+        {
+            PlayerUpgrades.maxHealthUp = 200 + Int32.Parse(tempCurrencyTxtList[1]);
+        }
         PlayerUpgrades.clonesUpgrade = 0 + Int32.Parse(tempCurrencyTxtList[2]);
         PlayerUpgrades.dashCooldownUpgrades = 0 + Int32.Parse(tempCurrencyTxtList[3]);
         PlayerUpgrades.haltUpgrades = 0 + Int32.Parse(tempCurrencyTxtList[4]);
         PlayerUpgrades.moveSpeedUpgrade = 0 + Int32.Parse(tempCurrencyTxtList[5]);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void toggleInvincibility()
+    {
+        if (invincibility == false)
+        {
+            invincibility = true;
+            invincibilityTxt.color = Color.green;
+        } else
+        {
+            invincibility = false;
+            invincibilityTxt.color = Color.red;
+        }
     }
 
     public void QuitGame ()
