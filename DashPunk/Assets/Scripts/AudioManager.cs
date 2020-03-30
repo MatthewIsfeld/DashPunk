060a06b5fb609269722d32bff7b03b265ca68e7b
Joshua Curtis <jbccurtis@gmail.com>
2020-03-29 23:07:51 -0400

    volume slider working

--- DashPunk/Assets/Scripts/AudioManager.cs
+++ DashPunk/Assets/Scripts/AudioManager.cs
@@ -16,7 +16,7 @@ public class AudioManager : MonoBehaviour
             s.source.clip = s.clip;
             s.source.volume = s.volume;
             s.source.loop = s.loop;
-            //s.source.outputAudioMixerGroup = s.mixer;
+            s.source.outputAudioMixerGroup = s.mixer;
         }
     }
 
